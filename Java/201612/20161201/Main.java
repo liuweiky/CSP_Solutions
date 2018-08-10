597a8eefc8e268587d780b2d62107a4cab566540
Heliovic <1005031096@qq.com>
2018-08-10 22:18:38 +0800

    20161201 (100)

---
+++ Java/201612/20161201/Main.java
@@ -0,0 +1,35 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		int n = scanner.nextInt();
+		List<Integer> numbers = new ArrayList<Integer>();
+		for (int i = 0; i < n; i++) {
+			numbers.add(scanner.nextInt());
+		}
+		String outstr = "";
+		for (int i = 0; i < n; i++) {
+			int above = 0;
+			int below = 0;
+			for (int j = 0; j < n; j++) {
+				if (numbers.get(i) > numbers.get(j))
+					below += 1;
+				else if (numbers.get(i) < numbers.get(j))
+					above += 1;
+			}
+			if (above == below && !outstr.contains(String.valueOf(i))) {
+				outstr += (numbers.get(i) + " ");
+			}
+		}
+		
+		if (outstr.length() == 0) {
+			System.out.println("-1");
+		} else {
+			System.out.println(outstr.trim());
+		}
+		
+	}
+}
