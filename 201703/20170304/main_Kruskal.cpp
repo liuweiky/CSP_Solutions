f39e255048fbbb25b7312714ec738c7958fafff1
Heliovic <1005031096@qq.com>
2018-09-04 19:27:27 +0800

    20170304 (90)
    Kruskal + 最小生成树，运行超时，待优化

---
+++ 201703/20170304/main_Kruskal.cpp
@@ -0,0 +1,71 @@
+#include <cstdio>
+#include <algorithm>
+#include <vector>
+
+using namespace std;
+
+struct Edge
+{
+    int from, to, cost;
+    Edge(int f, int t, int c): from(f), to(t), cost(c) {}
+};
+
+int father[100002];
+int N, M;
+
+vector<Edge*> edges;
+
+int GetFather(int a)
+{
+    while (true)
+        if (father[a] == a)
+            break;
+        else
+            a = father[a];
+    return a;
+}
+
+bool cmp(Edge* e1, Edge* e2)
+{
+    return e1->cost < e2->cost;
+}
+
+void UnionSet(int a, int b)
+{
+    if (GetFather(a) != GetFather(b))
+        father[GetFather(a)] = GetFather(b);
+}
+
+
+int main()
+{
+
+    scanf("%d %d", &N, &M);
+    for (int i = 0; i < M; i++)
+    {
+        int s, t, c;
+        scanf("%d %d %d", &s, &t, &c);
+        edges.push_back(new Edge(s, t, c));
+    }
+
+    for (int i = 0; i <= N; i++)
+        father[i] = i;
+
+    sort(edges.begin(), edges.end(), cmp);
+
+    int index = 0;
+    for (; index < M; index++)
+    {
+        Edge* e = edges[index];
+        if (father[e->from] == father[e->to])
+            continue;
+        else
+            UnionSet(e->from, e->to);
+        if (GetFather(1) == GetFather(N))
+            break;
+    }
+
+    printf("%d", index == M ? edges[M-1]->cost : edges[index]->cost);
+
+    return 0;
+}
