703d06cf7a02a529ceb6c4519d79e07e1aeaa0bc
Heliovic <1005031096@qq.com>
2018-09-10 22:48:06 +0800

    20171204 (90)
    int -> long long

--- 201712/20171204/main.cpp
+++ 201712/20171204/main.cpp
@@ -10,13 +10,13 @@
 using namespace std;
 
 // 0: cost, 1: width
-int graph[MAX_N][MAX_N][2];
+long long graph[MAX_N][MAX_N][2];
 int N, M;
 
 int visit[MAX_N];
-int dis[MAX_N];
-int prev_width[MAX_N];
-int prev_cost[MAX_N];   // v 点前的narrow长度之平方
+long long dis[MAX_N];
+long long prev_width[MAX_N];
+long long prev_cost[MAX_N];   // v 点前的narrow长度之平方
 
 void dijkstra()
 {
@@ -44,8 +44,8 @@ void dijkstra()
                 {
                     if (prev_width[v] == NARROW)
                     {
-                        int tmpd = dis[v] - prev_cost[v];
-                        int t = (sqrt(prev_cost[v]) + graph[v][j][0]) * (sqrt(prev_cost[v]) + graph[v][j][0]);
+                        long long tmpd = dis[v] - prev_cost[v];
+                        long long t = (sqrt(prev_cost[v]) + graph[v][j][0]) * (sqrt(prev_cost[v]) + graph[v][j][0]);
                         tmpd += t;
                         if (dis[j] > tmpd)
                         {
@@ -67,7 +67,7 @@ void dijkstra()
             }
         }
 
-        int min_dis = INT_MAX;
+        long long min_dis = LONG_LONG_MAX;
         for (int j = 2; j <= N; j++)
         {
             if (min_dis > dis[j] && visit[j] == 0)
@@ -83,13 +83,14 @@ int main()
 {
     fill_n(&graph[0][0][0], MAX_N * MAX_N * 2, 0);
     fill_n(visit, MAX_N, 0);
-    fill_n(dis, MAX_N, INT_MAX);
+    fill_n(dis, MAX_N, LONG_LONG_MAX);
     fill_n(prev_width, MAX_N, WIDE);
     fill_n(prev_cost, MAX_N, 0);
     scanf("%d %d", &N, &M);
     for (int i = 1; i <= M; i++)
     {
-        int k, s, t, c;
+        int s, t;
+        int k, c;
         scanf("%d %d %d %d", &k, &s, &t, &c);
         graph[s][t][0] = graph[t][s][0] = c;
         graph[s][t][1] = graph[t][s][1] = k;
@@ -97,6 +98,6 @@ int main()
 
     dijkstra();
 
-    printf("%d", dis[N]);
+    printf("%lld", dis[N]);
     return 0;
 }
